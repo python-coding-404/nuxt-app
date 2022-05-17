0c2357b9e955f580681227f56231dc71f9cc9223
xbabhahkckjclkjclcjjclacjjclcjclc <lol@gmail.com>
2022-05-17 17:51:50 +0400

    Blah Blah GPL-3.0

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
 <template>
   <Tutorial/>
+  <p>
+	Alex Joseph, hereby disclaims all copyright interest in the program “nuxt-app” written by Alex Joseph.
+
+signature of Alex Joseph 5/17/2022
+Alex Joseph, Writer
+</p>
+
 </template>
 
 <script>
+/*
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 export default {
   name: 'IndexPage'
 }
